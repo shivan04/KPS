3cdf54e1488757be6328b3139545b7fdfb1494d6
Heelmatt21 <m.sahit@unasat.sr>
2021-03-01 12:57:18 -0300

    fragments

--- app/src/main/java/sr/unasat/kpsfinetracker/MainActivity.java
+++ app/src/main/java/sr/unasat/kpsfinetracker/MainActivity.java
@@ -1,16 +1,44 @@
 package sr.unasat.kpsfinetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-import sr.unasat.kpsfinetracker.R;
+import sr.unasat.kpsfinetracker.fragments.LicencePlateFragment;
+import sr.unasat.kpsfinetracker.fragments.MissingPersonsListFragment;
+import sr.unasat.kpsfinetracker.fragments.MostWantedListFragment;
+import sr.unasat.kpsfinetracker.fragments.SectionsStatePagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private SectionsStatePagerAdapter mSectionsStatePagerAdapter;
+    private ViewPager mViewPager;
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Started.");
+        mSectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager)findViewById(R.id.container);
+        //setup the pager
+        setupViewPager(mViewPager);
+
+    }
+    private void setupViewPager(ViewPager viewPager){
+        SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new LicencePlateFragment(), "Fragment1");
+        adapter.addFragment(new MostWantedListFragment(), "Fragment2");
+        adapter.addFragment(new MissingPersonsListFragment(), "Fragment3");
+        viewPager.setAdapter(adapter);
+
+    }
+    public void setViewPager(int fragmentNumber){
+        mViewPager.setCurrentItem(fragmentNumber);
     }
 }
---
+++ app/src/main/java/sr/unasat/kpsfinetracker/fragments/MostWantedListFragment.java
@@ -0,0 +1,69 @@
+package sr.unasat.kpsfinetracker.fragments;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import sr.unasat.kpsfinetracker.MainActivity;
+import sr.unasat.kpsfinetracker.R;
+
+public class MostWantedListFragment extends Fragment {
+    private static final String TAG = "MostWantedListFragment";
+
+    private Button btnNavFrag1;
+    private Button btnNavFrag2;
+    private Button btnNavFrag3;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.mostwantedlistfragment_layout, container, false);
+        btnNavFrag1 = (Button) view.findViewById(R.id.btnNavfrag1);
+        btnNavFrag2 = (Button) view.findViewById(R.id.btnNavfrag2);
+        btnNavFrag3 = (Button) view.findViewById(R.id.btnNavfrag3);
+        Log.d(TAG, "onCreateView: started.");
+
+        btnNavFrag1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Going to Licence plate search", Toast.LENGTH_SHORT).show();
+                //navigate to fragment method called
+
+                ((MainActivity)getActivity()).setViewPager(0);
+
+            }
+        });
+        btnNavFrag2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Going to Most wanted list", Toast.LENGTH_SHORT).show();
+                //navigate to fragment method called
+                ((MainActivity)getActivity()).setViewPager(1);
+
+            }
+        });
+        btnNavFrag3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Going to Missing persons list", Toast.LENGTH_SHORT).show();
+                //navigate to fragment method called
+                ((MainActivity)getActivity()).setViewPager(2);
+
+            }
+        });
+
+
+        return view;
+    }
+
+
+
+}
