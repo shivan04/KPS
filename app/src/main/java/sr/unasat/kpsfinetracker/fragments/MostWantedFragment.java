4e342505d20793e6a05bc8ffa0705ff4ddb81441
shivan <s.sewdien@unasat.sr>
2021-04-10 18:37:45 -0300

    logout en sidemenu

--- app/src/main/java/sr/unasat/kpsfinetracker/fragments/MostWantedFragment.java
+++ app/src/main/java/sr/unasat/kpsfinetracker/fragments/MostWantedFragment.java
@@ -1,41 +1,28 @@
 package sr.unasat.kpsfinetracker.fragments;
 
 import android.os.Bundle;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-
 
 import sr.unasat.kpsfinetracker.R;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FileDownloadTask;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
-import java.io.File;
 
 
 public class MostWantedFragment extends Fragment {
 
-    private StorageReference mStorageRef;
-    private ImageView mostwantedlist;
 
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate (R.layout.fragment_most_wanted, container, false);
 
+
     }
-
-
-
-
-
 }
+
